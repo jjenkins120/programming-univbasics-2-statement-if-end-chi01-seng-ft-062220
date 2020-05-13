92a21f7f624fc072cf86675e5b687fa90ac20c12
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-13 18:26:02 +0000

    Automatically backed up by Learn

--- demo.rb
+++ demo.rb
@@ -1,7 +1,8 @@
 chance_of_rain = 0.2 
-if chance_of_rain > 0.5 
+if chance_of_rain <= 0.25 
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "Pack an umbrella!"
-else 
-  puts "Enjoy the fine day!"
+else
+puts "Stay home and read Hegel."
 end
-puts "Oh, and always wear sunscreen!"
